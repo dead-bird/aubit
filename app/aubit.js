56a9af84ac41fb1a16e416e4caae4b07492c1ad2
DaveP <dave@wearecrystal.uk>
2018-01-23 22:32:36 +0000

    Added FS. Playing around with conf/scaffolding process out

--- app/aubit.js
+++ app/aubit.js
@@ -1,61 +1,65 @@
 require('dotenv').config();
 
-let blessed = require('blessed'),
+let fs = require('fs'),
+    blessed = require('blessed'),
     contrib = require('blessed-contrib'), // not in use yet
     screen = blessed.screen(),
-    env = process.env;
+    env = process.env,
+    file = './app/conf.json',
+    conf = {user:'', access:'', refresh:''},
+    arg = process.argv.slice(2);
 
-let ascii = `
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdddmmmmmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddmmmmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddmmmmmmddmmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdysshmmmddmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmddmmmmh:::/mmmmddmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmdddmmdyosymmmmmddmmmmmmm
-mmmmmmmmmmddddddddddmmmmmmmmddmmmmmddmmmmmddmmmmmm
-mmmmmmmdddhysoooosyhhdddmmmmmddmmmmmddmmmmmddmmmmm
-mmmmmddhy+:-..'''..-/ohddmmmmmmdmmmmmddmmmmmdmmmmm
-mmmmddy+-.'''''''''''.:shddmmmmddmmmmmdmmmmmdmmmmm
-mmmddy/.'''''''''''''''-ohdmmmmdhdmmmmdmmmmmddmmmm
-mmmdh+.''''''''''''''''.:sddmmdooosmmmdmmmmmmdmmmm
-mmddy/.'''''''''''''''''-shdmmdsooymmmddmmmmmdmmmm
-mmmdh+.''''''''''''''''.:sddmmmmddmmmmdmmmmmmdmmmm
-mmmdds:.'''''''''''''''-ohdmmmmmdmmmmmdmmmmmddmmmm
-mmmmddy/.''''''''''''.-ohdmmmmmddmmmmddmmmmmdmmmmm
-mmmmmddhs/-..'''''..:+yddmmmmmddmmmmmdmmmmmddmmmmm
-mmmmmmmddhyso++++osyhdddmmmmmddmmmmmddmmmmmdmmmmmm
-mmmmmmmmmdddddddddddmmmmmmmmddmmmmmddmmmmmddmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmdddmmmmdddmmmmmddmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmddmmmmmddmmmmmmddmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdddmmmdhyhdmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmddmmmmmssssdmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmddhhhmmmmmmmmmm
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmdddmmmmmmmmmmmmmm
-`
+if (arg) {
+  auth()
+}
 
-screen.title = 'title of song I guess?';
+function auth() {
+  console.log(`https://accounts.spotify.com/en/authorize?response_type=code&client_id=${env.CLIENT}&redirect_uri=${env.REDIRECT}&scope=${env.SCOPE}`);
+}
 
-let box = blessed.box({
-  top: 'center',
-  left: 'center',
-  width: '100%',
-  height: '100%',
-  content: ascii,
-  tags: true,
-  align: 'center',
-  style: {
-    fg: 'white',
-  }
-});
+// if (conf.user) {
+//   run();
+// } else {
+//   console.log('visit http://www.example.com and grab the generated token. Paste that badboy in conf.json and re-run the app');
+// }
 
-screen.append(box);
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
+// fs.writeFile(conf, JSON.stringify({access:'test', refresh:'test'}), 'utf8', function(err) {
+//   if(err) throw err;
+// });
 
-box.focus();
+// fs.readFile(conf, function(err, content) {
+//   if(err) throw err;
 
-screen.render();
+//   let data = JSON.parse(content);
+
+//   console.log(data.access);
+// });
+
+
+// screen.title = 'title of song I guess?';
+
+// let box = blessed.box({
+//   top: 'center',
+//   left: 'center',
+//   width: '100%',
+//   height: '100%',
+//   content: ascii,
+//   tags: true,
+//   align: 'center',
+//   style: {
+//     fg: 'white',
+//   }
+// });
+
+// screen.append(box);
+
+// screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+//   return process.exit(0);
+// });
+
+// box.focus();
+
+// screen.render();
 
 console.log('whattup, it\'s me, ya boy');
