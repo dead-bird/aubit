7e9bc11f495bb5a68ad81666625854404a798477
DaveP <dave@wearecrystal.uk>
2018-01-30 22:40:57 +0000

    Started working on auth flow for Spotify

--- app/aubit.js
+++ app/aubit.js
@@ -3,23 +3,21 @@ require('dotenv').config();
 let fs = require('fs'),
     blessed = require('blessed'),
     contrib = require('blessed-contrib'), // not in use yet
-    axios = require('axios'),
     List = require('prompt-list'),
     // screen = blessed.screen(),
     env = process.env,
     path = './app/config/',
-    base = 'https://accounts.spotify.com/api',
     conf;
 
 fs.readFile(path + 'conf.json', (err, content) => {
-  if(err) throw err;
+  if (err) throw err;
 
   conf = JSON.parse(content);
 
   if (conf.first) {
     setup();
   } else {
-    reset();
+    run();
   }
 });
 
@@ -44,11 +42,12 @@ function setup() {
 }
 
 function run() {
-  let module = require(`./services/${conf.preferred}.js`)
-  let service = new module;
+  let Module = require(`./services/${conf.preferred}.js`);
+  let Service = new Module;
 
-  service.init();
+  Service.auth(conf);
 }
+
 
 function save() {
   fs.writeFile(path + 'conf.json', JSON.stringify(conf), 'utf8', (err) => {
@@ -67,43 +66,8 @@ function reset() {
 }
 
 
-// if (arg) {
-//   auth(arg);
-// }
-
-// function auth(code) {
-//   axios.get(`${base}/token`, {
-//       params: {
-//         code: code,
-//         redirect_uri: env.REDIRECT,
-//         grant_type: 'authorization_code'
-//       },
-//       headers: {
-//         'Authorization': 'Basic ' + (new Buffer(`${env.CLIENT}:${env.SECRET}`).toString('base64'))
-//       },
-//     })
-//     .then(function(res) {
-//       console.log(res);
-//     })
-//     .catch(function(err) {
-//       console.log(err);
-//     })
-// }
 
 
-
-
-
-
-
-
-
-
-// if (conf.user) {
-//   run();
-// } else {
-//   console.log('visit http://www.example.com and grab the generated token. Paste that badboy in conf.json and re-run the app');
-// }
 
 
 // screen.title = 'title of song I guess?';
--- app/services/spotify.js
+++ app/services/spotify.js
@@ -1,5 +1,72 @@
+let axios = require('axios'),
+    http = require('http'),
+    url = require('url'),
+    opn = require('opn'),
+    fs = require('fs'),
+    base = 'https://accounts.spotify.com/api',
+    env = process.env,
+    server,
+    conf;
+
 module.exports = class Spotify {
-  init() {
-    console.log('yeah boiiiiiiiii');
+  auth(config) {
+    conf = config;
+
+    if (conf.spotify.code) {
+      this.getRefresh();
+    } else {
+      this.getCode();
+    }
   }
+
+  getCode() {
+    opn('http://localhost:8080/?code=12345678');
+
+    server = http.createServer((req, res) => {
+      fs.readFile('./app/callback/callback.html', (err, data) => {
+        if (err) throw err;
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+
+        let query = url.parse(req.url, true).query;
+
+        if (query.code) {
+          conf.spotify.code = query.code;
+          this.getAccess();
+        }
+
+        res.end();
+      });
+    }).listen(8080);
+  }
+
+  getAccess() {
+    server.close(() => { process.exit() });
+
+    axios.get(`${base}/token`, {
+      params: {
+        code: conf.spotify.code,
+        redirect_uri: env.REDIRECT,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(`${env.CLIENT}:${env.SECRET}`).toString('base64'))
+      },
+    })
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  }
+
+  getRefresh() {
+    console.log('refresh')
+  }
+}
+
+
+function auth(code) {
 }
